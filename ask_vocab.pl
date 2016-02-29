924455eebb74019a2bf4fa046c8a9d523296d087
Ivan Sukin <muspellsson@undeadbsd.org>
2016-02-29 14:34:37 +0300

    vocab

--- ask_vocab.pl
+++ ask_vocab.pl
@@ -1,24 +1,82 @@
 :- module(ask_vocab,
 	[ask_vocab/1]).
 :- use_module(library(pce)).
+:- use_module(library(readutil)).
 :- encoding(utf8).
 
 ask_vocab(BB) :-
 	new(D, dialog('Справочник')),
-	send(D, append, new(D1, dialog_group(buttons, group))),
+	send(D, append, new(DD, dialog_group(buttons, group))),
+	send(DD, append, new(D1, dialog_group(buttons, group))),
 	send(D1, label, 'Теплоотдача пар-металл'),
 	send(D1, border, size(10, 10)),
 	send(D1, append, new(B1, browser)),
-	send(D, append, new(D2, dialog_group(buttons, group)), right),
+	send(DD, append, new(D2, dialog_group(buttons, group)), right),
 	send(D2, label, 'Теплоотдача металл-жидкость'),
 	send(D2, border, size(10, 10)),
 	send(D2, append, new(B2, browser)),
-	send(D, append, new(D3, dialog_group(buttons, group)), right),
+	send(DD, append, new(D3, dialog_group(buttons, group)), right),
 	send(D3, label, 'Теплопроводность металла'),
 	send(D3, border, size(10, 10)),
 	send(D3, append, new(B3, browser)),
-	%send(D, append, new(D4, dialog_group(buttons, group)), below),
 	send(D, append, new(N1, text_item('surface')), below),
 	send(N1, label, 'Площадь куба, м²:'),
-	send(D, append, button(ok)),
-	send(D, open).
+	send(D, append, button(ok, message(@prolog, ret, D, 
+		B1, B2, B3, N1?selection))),
+	fill_browser(1, B1),
+	fill_browser(2, B2),
+	fill_browser(3, B3),
+	send(D, default_button, ok),
+	get(D, confirm, Answer),
+	send(D, destroy),
+	Answer \== @nil,
+	BB = Answer.
+
+ret(D, B1, B2, B3, N1) :-
+	(
+		get(B1, selection, Sel1),
+		get(B2, selection, Sel2),
+		get(B3, selection, Sel3)
+	) -> 
+	(
+		(
+			get(Sel1, key, S1), get(Sel2, key, S2), get(Sel3, key, S3),
+			calculate(S1, S2, S3, N1, Res)
+		) -> send(D, return, Res);
+		send(D, return, @nil)
+	);
+	send(D, return, @nil).
+
+calculate(S1, S2, S3, S4, Res) :-
+	split_string(S1, "\t", " ", L1),
+	split_string(S2, "\t", " ", L2),
+	split_string(S3, "\t", " ", L3),
+	last(L1, La1),
+	last(L2, La2),
+	last(L3, La3),
+	number_string(No1, La1),
+	number_string(No2, La2),
+	number_string(No3, La3),
+	number_string(No4, S4),
+	Res = No1.	
+
+fill_browser(1, B) :-
+	open('vocab/vocab1.txt', read, F),
+	do_fill(F, B).
+
+fill_browser(2, B) :-
+	open('vocab/vocab2.txt', read, F),
+	do_fill(F, B).
+
+fill_browser(3, B) :-
+	open('vocab/vocab3.txt', read, F),
+	do_fill(F, B).
+
+do_fill(F, B) :-
+	read_line_to_codes(F, S),
+	(
+		S = end_of_file -> !;
+		atom_codes(A, S),
+		send(B, append, A),
+		do_fill(F, B)
+	).
--- binary.pl
+++ binary.pl
@@ -1,6 +1,7 @@
 :- use_module(library(pce)).
 :- use_module(library(help_message)).
 :- use_module(ask_mixture).
+:- use_module(ask_vocab).
 
 :- encoding(utf8).
 
@@ -11,34 +12,34 @@ variable(mupper, string, get, "Upper product mixture").
 variable(mlower, string, get, "Lower product mixture").
 
 initialise(D) :->
-	  send_super(D, initialise('Бинарная ректификация')),
-          send(D, clearvars),
-	  send(D, mixture_group),
-	  send(D, betad_group),
-	  send(D, betab_group),
-	  send(D, k_group),
-	  send(D, append, button('расчет')),
-	  send(D, append, button('помощь'), right),
-          send_super(D, open).
+	send_super(D, initialise('Бинарная ректификация')),
+	send(D, clearvars),
+	send(D, mixture_group),
+	send(D, betad_group),
+	send(D, betab_group),
+	send(D, k_group),
+	send(D, append, button('расчет')),
+	send(D, append, button('помощь'), right),
+	send_super(D, open).
 
 clearvars(D) :->
 	send(D, slot, mfeed, ""),
-        send(D, slot, mupper, ""),
+	send(D, slot, mupper, ""),
 	send(D, slot, mlower, "").
 
 mixture_group(D) :->
-        new(D1, dialog_group(buttons, group)),
-        send(D1, label, 'Компоненты'),
-        send(D1, border, size(10, 10)),
+	new(D1, dialog_group(buttons, group)),
+	send(D1, label, 'Компоненты'),
+	send(D1, border, size(10, 10)),
 	send(D1, size, size(400, 40)),
 	send(D1, elevation, 1),
 	new(B1, button('исходная_смесь')),
-        new(B2, button('верхний_продукт')),
-        new(B3, button('нижний_продукт')),
-        send(B1, message, message(D, get_feed, B1, mfeed)),
-        send(B2, message, message(D, get_feed, B2, mupper)),
-        send(B3, message, message(D, get_feed, B3, mlower)),
-        send(D1, append, B1),
+	new(B2, button('верхний_продукт')),
+	new(B3, button('нижний_продукт')),
+	send(B1, message, message(D, get_feed, B1, mfeed)),
+	send(B2, message, message(D, get_feed, B2, mupper)),
+	send(B3, message, message(D, get_feed, B3, mlower)),
+	send(D1, append, B1),
 	send(D1, append, B2, right),
 	send(D1, append, B3, right),
 	send(B1, help_message, tag, 'Пусто'),
@@ -47,53 +48,57 @@ mixture_group(D) :->
 	send(D, append, D1).
 
 betad_group(D) :->
-        new(D1, dialog_group(buttons, group)),
-        send(D1, label, 'Коэффициент теплопереноса в дефлегматоре, кВт/К'),
-        send(D1, border, size(10, 10)),
+	new(D1, dialog_group(buttons, group)),
+	send(D1, label, 'Коэффициент теплопереноса в дефлегматоре, кВт/К'),
+	send(D1, border, size(10, 10)),
 	send(D1, size, size(400, 80)),
 	send(D1, elevation, 1),
 	new(C, text_item('bD')),
 	send(C, label, 'βᴰ:'),
-        send(D1, append, C),
-        send(D1, append, button('по_справочнику')),
-        send(D1, append, button('по_известным_параметрам'), right),
-        send(D, append, D1).
+	send(D1, append, C),
+	send(D1, append, button('по_справочнику', message(D, vocab, C))),
+	send(D1, append, button('по_известным_параметрам'), right),
+	send(D, append, D1).
 
 betab_group(D) :->
 	new(D1, dialog_group(buttons, group)),
-        send(D1, label, 'Коэффициент теплопереноса в кубе, кВт/К'),
-        send(D1, border, size(10, 10)),
+	send(D1, label, 'Коэффициент теплопереноса в кубе, кВт/К'),
+	send(D1, border, size(10, 10)),
 	send(D1, size, size(400, 80)),
 	send(D1, elevation, 1),
 	new(C, text_item('bB')),
 	send(C, label, 'βᴮ:'),
-        send(D1, append, C),
-	send(D1, append, button('по_справочнику')),
-        send(D1, append, button('по_известным_параметрам'), right),
-        send(D, append, D1).
+	send(D1, append, C),
+	send(D1, append, button('по_справочнику', message(D, vocab, C))),
+	send(D1, append, button('по_известным_параметрам'), right),
+	send(D, append, D1).
+
+vocab(D, C) :->
+	ask_vocab(Coeff),
+	send(C, selection, Coeff).
 
 k_group(D) :->
-        new(D1, dialog_group(buttons, group)),
-        send(D1, label, 'Коэффициент массопереноса'),
-        send(D1, border, size(10, 10)),
+	new(D1, dialog_group(buttons, group)),
+	send(D1, label, 'Коэффициент массопереноса'),
+	send(D1, border, size(10, 10)),
 	send(D1, size, size(400, 100)),
 	send(D1, elevation, 1),
 	new(C, text_item('k')),
 	send(C, label, 'k:'),
-        send(D1, append, C),
-        send(D1, append, button('по_формуле')),
-        send(D1, append, button('по_одному_режиму'), right),
+	send(D1, append, C),
+	send(D1, append, button('по_формуле')),
+	send(D1, append, button('по_одному_режиму'), right),
 	send(D1, append, button('по_двум_режимам'), below),
 	send(D1, append, button('по_трем_и_более_режимам'), right),
-        send(D, append, D1).
+	send(D, append, D1).
 
 
 get_feed(D, B, Var) :->
 	get(D, Var, F),
-        get(F, value, F0),
-        atom_string(F0, F1),
-        send(@prolog, ask_mixture, M, F1),
+	get(F, value, F0),
+	atom_string(F0, F1),
+	send(@prolog, ask_mixture, M, F1),
 	send(B, help_message, tag, M),
-        send(D, slot, Var, M).
+	send(D, slot, Var, M).
 
 :- pce_end_class.
